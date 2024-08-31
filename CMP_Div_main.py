dfa23264bdf9b5f19dc5201716e94bc3a25f45c3
WD8844 <whitedragon8844@gmail.com>
2024-08-31 11:36:49 +0800

    Update CMP_Div_main.py
    #

--- CMP_Div_main.py
+++ CMP_Div_main.py
@@ -10,14 +10,14 @@ def main(jpdir,chdir,nstr,method = None,path = None,blocknums = 2,encoding = 'ut
         Div_Restore(CMPdirpath,blocknums = blocknums,encoding = encoding,fmnewdir = newdir)
         MakeCMP_TH(CMPdirpath,encoding = encoding,mknewdir = newdir)
 
-if __name__ == "__main__":
+if __name__ == "__main__":#Just for the code test.
     for num in range(2):
         nstr = str(num+2) + '_extr'
         jpdir = 'B(JP)'
         chdir = 'B(CH)'
-        #分
+        #divide
         #main(jpdir,chdir,nstr,method = "CMP",path = None,blocknums = 2,encoding = 'utf-16',newdir = True)
-        #合
+        #combine
         ft = jpdir[0] + nstr[0] + "_CMP"
         CMPdirpath = jpdir + nstr+"/" + ft
         main(jpdir,chdir,nstr,method = "Restore",path = CMPdirpath,blocknums = 2,encoding = 'utf-16',newdir = True)
